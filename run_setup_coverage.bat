3baee2d35899b42e67404c5780c44738586fb61b
saullocastro <saullogiovani@gmail.com>
2019-07-25 17:43:48 +0200

    TST: attempt to run coveralls in Cython

--- run_setup_coverage.bat
+++ run_setup_coverage.bat
@@ -1,1 +1,1 @@
-python setup.py build_ext --inplace --force --define CYTHON_TRACE --define NPY_NO_DEPRECATED_API
+python setup.py build_ext --inplace --force --define CYTHON_TRACE_NOGIL --define NPY_NO_DEPRECATED_API
