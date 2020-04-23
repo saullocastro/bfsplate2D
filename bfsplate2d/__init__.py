22c88eae57bf3e5c59e39cb411c6098858d41746
saullocastro <saullogiovani@gmail.com>
2020-04-23 12:28:22 +0200

    DEV: sparse implementation

--- bfsplate2d/__init__.py
+++ bfsplate2d/__init__.py
@@ -4,6 +4,7 @@ BFSPLATE2D - Implementation of the BFS plate finite element in 2D
 Author: Saullo G. P. Castro
 
 """
-from .bfsplate2d import BFSPlate2D, update_K, update_Kg, update_M
+from .bfsplate2d import BFSPlate2D, update_KC0, update_KG, update_M
+from .bfsplate2d import INT, DOUBLE, KC0_SPARSE_SIZE, KG_SPARSE_SIZE, M_SPARSE_SIZE
 DOF = 6
 
